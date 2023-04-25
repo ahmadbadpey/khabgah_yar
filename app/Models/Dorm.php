52e5f094967faf451941db7f6259a90a1023eb0a
Ahmad <ahmadbadpey@gmail.com>
2023-04-25 14:49:18 +0330

    some Models Added

---
+++ app/Models/Dorm.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+
+class Dorm extends Model
+{
+    use HasFactory;
+
+    public function city(): HasOne
+    {
+        return $this->hasOne(City::class);
+    }
+
+}
